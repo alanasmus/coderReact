1d74688e9ce881cc4f63e463d1df0c35fb276257
alan asmus <alan94vb@gmail.com>
2022-07-05 17:57:27 -0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="navbar navbar-expand-lg">
+          <div className="container-fluid">
+            <a className="navbar__brand" href="./public/index.html">Libreria Luar</a>
+            <div class="collapse navbar-collapse navbar__link__list" id="navbarNavAltMarkup">
+              <div class="navbar__link__list__a">
+                <a class="navbar__link__list__a__link" aria-current="page" href="#">Escolar</a>
+                <a class="navbar__link__list__a__link" href="#">Comercial</a>
+                <a class="navbar__link__list__a__link" href="#">Artistica</a>
+              </div>
+            </div>
+          </div>
+        </nav>
       </header>
     </div>
   );
