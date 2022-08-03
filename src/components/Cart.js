6e50b7c236569cff45a551afdab2c695df1582e7
alan asmus <alan94vb@gmail.com>
2022-08-02 23:20:26 -0300

    cartView

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useState } from 'react';
 import { CartContext } from './CartContext';
 import { Link } from 'react-router-dom';
 import { Button } from '@mui/material';
