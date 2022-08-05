749c3954226007bea02a65f7b06097d874407750
alan asmus <alan94vb@gmail.com>
2022-08-04 23:34:35 -0300

    itemCollection

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
-
+import { Button } from '@mui/material';
 
 const Item = (props) => {
     return (
@@ -16,7 +16,7 @@ const Item = (props) => {
                     <h3>{props.price}</h3>
                 </div>
                 <div style={{ cursor: "pointer" }} className="card-text card-container-link">
-                    <Link to={`/item/${props.id}`}>Detalles</Link>
+                    <Link to={`/item/${props.id}`}><Button variant="contained">Detalles</Button></Link>
                 </div>
             </div>
         </div>
